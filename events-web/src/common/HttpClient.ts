bd8a42d2ce55c3dfe6e4e9cdccb98411a3c9a385
Nguyen Danh Ha <ndh103@gmail.com>
2022-09-06 22:15:13 +0700

    add bearer when calling api

--- events-web/src/common/HttpClient.ts
+++ events-web/src/common/HttpClient.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 import { globalConfig } from '@/configs/config'
+import { msalInstance } from '@/main'
+import { loginRequest } from '@/configs/authConfig'
 
 let hasInstance = false
 let httpClient: AxiosInstance
@@ -9,6 +11,44 @@ const HttpClient = () => {
     httpClient = axios.create({
       baseURL: globalConfig.config.apiUrl,
     })
+
+    // Add a request interceptor
+    httpClient.interceptors.request.use(
+      async (config) => {
+        const account = msalInstance.getActiveAccount()
+        if (!account) {
+          throw Error('No active account! Verify a user has been signed in and setActiveAccount has been called.')
+        }
+
+        const response = await msalInstance.acquireTokenSilent({
+          ...loginRequest,
+          account,
+        })
+
+        const { accessToken } = response
+
+        // eslint-disable-next-line no-param-reassign
+        config.headers!.Authorization = `Bearer ${accessToken}`
+
+        // Do something before request is sent
+        return config
+      },
+      (error) =>
+        // Do something with request error
+        Promise.reject(error)
+    )
+
+    // Add a response interceptor
+    httpClient.interceptors.response.use(
+      (response) =>
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        response,
+      (error) =>
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        Promise.reject(error)
+    )
 
     hasInstance = true
   }
