36d7960cca383c5c48d30cd1110e13385c6fa5ec
Nguyen Danh Ha <ndh103@gmail.com>
2022-10-14 22:07:56 +0700

    Add OpenAPi

--- events-authorization-api/Events.Authorization.Api/Program.cs
+++ events-authorization-api/Events.Authorization.Api/Program.cs
@@ -1,9 +1,11 @@
 using Events.Authorization.Api.Common.Configs;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureOpenApi()
     .ConfigureServices((context, services) =>
     {
         var configuration = context.Configuration;
@@ -11,6 +13,7 @@ var host = new HostBuilder()
         services.Configure<AzureB2CConfig>(configuration.GetSection(nameof(AzureB2CConfig)));
 
     })
+    
     .Build();
 
 host.Run();
